68b3790e74912ca3c9137ab8f857a5ded589688e
michaelajuarez <michaelajuarez@gmail.com>
2021-07-21 01:50:35 -0700

    Added beginning of navbar

--- frontend/src/Channels.js
+++ frontend/src/Channels.js
@@ -1,5 +1,13 @@
+import {BottomNavigation} from '@material-ui/core';
 import React from 'react';
 import {useHistory} from 'react-router-dom';
+import {makeStyles} from '@material-ui/core/styles';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import HomeIcon from '@material-ui/icons/Home';
+import ForumIcon from '@material-ui/icons/Forum';
+import AlternateEmailIcon from '@material-ui/icons/AlternateEmail';
+import SearchIcon from '@material-ui/icons/Search';
+import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
 
 /**
  *
@@ -72,6 +80,22 @@ function fetchMessages(setMessages) {
     });
 } */
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    position: 'absolute',
+    height: '100%',
+    width: '100%',
+    // bottom: 0,
+    margin: 0,
+  },
+  bottomNav: {
+    position: 'fixed',
+    bottom: '0%',
+    margin: 'auto',
+    width: '100%',
+  },
+}));
+
 /**
  *
  * @return {object}
@@ -85,12 +109,14 @@ function Channels() {
   // let [messageDisplay, setMessageDisplay] = React.useState(false);
   // const [messages, setMessages] = React.useState({});
 
+  const classes = useStyles();
+
   React.useEffect(() => {
     fetchChannels(setChannels /* , setError */);
   }, []);
 
   return (
-    <div>
+    <div className={classes.root}>
       <h2 id='category'>Channels</h2>
       <div id='channels'>
         {channels.map((channel) => {
@@ -119,6 +145,20 @@ function Channels() {
       <div id='channels'>
       </div>
       <button onClick={() => history.push('/channels')}>Home</button>
+      <BottomNavigation
+        className={classes.bottomNav}>
+        {/* value={value} onChange={handleChange} className={classes.root}> */}
+        <BottomNavigationAction
+          label="Home" value="home" icon={<HomeIcon />} />
+        <BottomNavigationAction
+          label="Messages" value="messages" icon={<ForumIcon />} />
+        <BottomNavigationAction
+          label="At" value="at" icon={<AlternateEmailIcon />} />
+        <BottomNavigationAction
+          label="Search" value="search" icon={<SearchIcon />} />
+        <BottomNavigationAction
+          label="Profile" value="profile" icon={<PersonOutlineIcon />} />
+      </BottomNavigation>
     </div>
   );
 };
