a54a4dbaaf9eb8b4223d1d7fa2b9710782147a14
michaelajuarez <miajuare@ucsc.com>
2021-07-22 22:02:03 -0700

    Added final touches to DMS

--- frontend/src/DMs.js
+++ frontend/src/DMs.js
@@ -1,5 +1,28 @@
 import React from 'react';
 import {useHistory} from 'react-router-dom';
+import {makeStyles} from '@material-ui/core/styles';
+import {BottomNavigation} from '@material-ui/core';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import HomeIcon from '@material-ui/icons/Home';
+import ForumIcon from '@material-ui/icons/Forum';
+import AlternateEmailIcon from '@material-ui/icons/AlternateEmail';
+import SearchIcon from '@material-ui/icons/Search';
+import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+// import TextField from '@material-ui/core/TextField';
+// import InputAdornment from '@material-ui/core/InputAdornment';
+// import SendIcon from '@material-ui/icons/Send';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+// import ReplyIcon from '@material-ui/icons/Reply';
+// import Divider from '@material-ui/core/Divider';
 
 /**
  *
@@ -34,6 +57,32 @@ function fetchMessages(setMessages) {
     });
 }
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    position: 'absolute',
+    height: '100%',
+    width: '100%',
+    margin: 0,
+    left: 0,
+    top: 0,
+  },
+  bottomNav: {
+    position: 'fixed',
+    bottom: 0,
+    margin: 'auto',
+    width: '100%',
+  },
+  list: {
+    margin: 0,
+  },
+  inputText: {
+    position: 'fixed',
+    bottom: 60,
+    width: '100%',
+  },
+}));
+
+
 /**
  *
  * @return {object}
@@ -41,6 +90,7 @@ function fetchMessages(setMessages) {
 function DMs() {
   const history = useHistory();
   const item = localStorage.getItem('user');
+  const classes = useStyles();
   if (!item) {
     history.push('/');
   }
@@ -77,16 +127,51 @@ function DMs() {
   }, []);
 
   return (
-    <div>
+    <div className={classes.root}>
+      <AppBar position="static" style={{backgroundColor: '#39123e'}}>
+        <Toolbar>
+          <IconButton edge="start"
+            className={classes.menuButton} color="inherit" aria-label="menu"
+            onClick={() => history.push('/channels')}>
+            <ArrowBackIcon/>
+          </IconButton>
+          <Typography variant="h6" component={'div'}>Messages</Typography>
+        </Toolbar>
+      </AppBar>
+
       {messages.sort((a, b) => (
         a.createdtime > b.createdtime) ? 1 : -1).map((message, index) => (
         <div key={index}>
-          <p>{message.createdby} : {timeStamp(message.createdtime)}</p>
-          <p>{message.content}</p>
+          <List component='nav'
+            aria-label='main mailbox folders' key={index}>
+            <ListItem>
+              <ListItemIcon>
+                <AccountCircleIcon />
+              </ListItemIcon>
+              <ListItemText
+                primary={message.createdby+' : '+timeStamp(message.createdtime)}
+                secondary={message.content}
+              />
+            </ListItem>
+          </List>
         </div>
       ))}
-      <button onClick={() => history.push('/channels')}>Home</button>
-      <button onClick={() => history.push('/dms')}>DMs</button>
+
+      <BottomNavigation
+        className={classes.bottomNav}>
+        <BottomNavigationAction
+          label="Home" value="home" icon={<HomeIcon />}
+          onClick={() => history.push('/channels')}/>
+        <BottomNavigationAction
+          label="Messages" value="messages" icon={<ForumIcon />}
+          onClick={() => history.push('/dms')} />
+        <BottomNavigationAction
+          label="At" value="at" icon={<AlternateEmailIcon />} />
+        <BottomNavigationAction
+          label="Search" value="search" icon={<SearchIcon />} />
+        <BottomNavigationAction
+          label="Profile" value="profile" icon={<PersonOutlineIcon />} />
+      </BottomNavigation>
     </div>
   );
 };
