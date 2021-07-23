8f9e841578d1ad8452f17b23bac7a3da3b5858cd
michaelajuarez <michaelajuarez@gmail.com>
2021-07-22 22:35:28 -0700

    Finished adding sources

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,12 @@
 /**
  * Sources:
+ * Lecture Slides/Notes
  * https://stackoverflow.com/questions/52401960/multiline-secondary-text-for-list-react-component
  * https://material-ui.com/
  * https://code-boxx.com/url-parts-javascript/
+ * https://www.postgresqltutorial.com/postgresql-json/
+ * https://www.w3schools.com/sql/sql_insert_into_select.asp#:~:text=The%20INSERT%20INTO%20SELECT%20statement,the%20target%20table%20are%20unaffected.
+ * https://www.postgresql.org/docs/8.2/tutorial-join.html
  */
 
 import React from 'react';
